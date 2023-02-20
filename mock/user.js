bcd51a798714886ab481e2d69c2b98bf1fccb140
zhoudongdong666 <936048983@qq.com>
2023-02-21 00:01:21 +0800

    模拟登录登出接口

--- mock/user.js
+++ mock/user.js
@@ -1,11 +1,10 @@
-
 const tokens = {
   admin: {
-    token: 'admin-token'
+    token: 'admin-token',
   },
   editor: {
-    token: 'editor-token'
-  }
+    token: 'editor-token',
+  },
 }
 
 const users = {
@@ -13,72 +12,157 @@ const users = {
     roles: ['admin'],
     introduction: 'I am a super administrator',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'Super Admin'
+    name: 'Super Admin',
   },
   'editor-token': {
     roles: ['editor'],
     introduction: 'I am an editor',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
-    name: 'Normal Editor'
-  }
+    name: 'Normal Editor',
+  },
 }
 
 module.exports = [
-  // user login
+  // 1  user login
   {
-    url: '/vue-admin-template/user/login',
+    url: '/login',
     type: 'post',
-    response: config => {
-      const { username } = config.body
-      const token = tokens[username]
+    response: (config) => {
+      // config.body = {
+      //   principal: 'admin', // 账号
+      //   credentials: '123456', // 密码
+      //   sessionUUID: 'dba48f0c-4140-d4f6-344d-08cc3cfea0f3',
+      //   validCode: 'znue',
+      // }
+      const { principal } = config.body
+      const token = tokens[principal]
 
       // mock error
       if (!token) {
         return {
           code: 60204,
-          message: 'Account and password are incorrect.'
+          message: 'Account and password are incorrect.',
         }
       }
 
       return {
-        code: 20000,
-        data: token
+        // code: 20000,
+        // data: token,
+        ccess_token: '74c8499c-2d59-4cb5-8aa9-0ab679a82af7',
+        authorities: [],
+        expires_in: 43199,
+        refresh_token: '8d002439-7472-4bf7-b749-8ce25650cd55',
+        token_type: 'bearer',
+        userId: 'f5622d52c8164fcd8348dccfd840f624',
+        userStatus: '0',
       }
-    }
+    },
   },
 
   // get user info
   {
-    url: '/vue-admin-template/user/info\.*',
+    url: '/corp/user/info.*',
     type: 'get',
-    response: config => {
-      const { token } = config.query
-      const info = users[token]
+    response: (config) => {
+      // const { token } = config.query
+      // const info = users[token]
 
-      // mock error
-      if (!info) {
-        return {
-          code: 50008,
-          message: 'Login failed, unable to get user details.'
-        }
-      }
+      // // mock error
+      // if (!info) {
+      //   return {
+      //     code: 50008,
+      //     message: 'Login failed, unable to get user details.',
+      //   }
+      // }
 
       return {
-        code: 20000,
-        data: info
+        // code: 20000,
+        // data: info,
+
+        code: 0,
+        message: 'SUCCESS',
+        data: {
+          loginUser: 'admin',
+          userName: '海之云',
+          userMobile: '18051110000',
+          userMail: 'admin@hzy.com',
+          userPic: 'https://weboss.qmprint.cn/2023/01/fe263f8dd79b42388becaedf464c4b65.jpg',
+          userMemo: '海之云管理员',
+          status: 0,
+          corpName: '南京XXXXX有限责任公司',
+          userType: 1,
+          userRegtime: '2022-12-29 11:19:08',
+          userLasttime: '2023-02-06 22:23:27',
+          userLastip: '',
+          authMenu: null,
+          authWh: null,
+        },
       }
-    }
+    },
+  },
+
+  // user navMenu
+  {
+    url: '/corp/user/navMenu',
+    type: 'get',
+    response: (_) => {
+      return {
+        code: 0,
+        message: 'SUCCESS',
+        data: [
+          { menuCode: 'm10000', menuName: '库存管理', menuLevel: '1' },
+          { menuCode: 'm11000', menuName: '入库管理', menuLevel: '2' },
+          { menuCode: 'm12000', menuName: '出库管理', menuLevel: '2' },
+          { menuCode: 'm12100', menuName: '发起出库', menuLevel: '3' },
+          { menuCode: 'm12200', menuName: '出库审核', menuLevel: '3' },
+          { menuCode: 'm13000', menuName: '调拨管理', menuLevel: '2' },
+          { menuCode: 'm13100', menuName: '发起调拨', menuLevel: '3' },
+          { menuCode: 'm13200', menuName: '审核调拨', menuLevel: '3' },
+          { menuCode: 'm14000', menuName: '盘点管理', menuLevel: '2' },
+          { menuCode: 'm14100', menuName: '发起盘点', menuLevel: '3' },
+          { menuCode: 'm14200', menuName: '盘点审核', menuLevel: '3' },
+          { menuCode: 'm20000', menuName: '报表统计', menuLevel: '1' },
+          { menuCode: 'm21000', menuName: '库存统计', menuLevel: '2' },
+          { menuCode: 'm21100', menuName: '当前库存', menuLevel: '3' },
+          { menuCode: 'm21200', menuName: '库存统计', menuLevel: '3' },
+          { menuCode: 'm26000', menuName: '物流人员统计', menuLevel: '2' },
+          { menuCode: 'm22000', menuName: '入库纪录', menuLevel: '2' },
+          { menuCode: 'm23000', menuName: '出库纪录', menuLevel: '2' },
+          { menuCode: 'm24000', menuName: '调拨纪录', menuLevel: '2' },
+          { menuCode: 'm25000', menuName: '盘点纪录', menuLevel: '2' },
+          { menuCode: 'm27000', menuName: '台账管理', menuLevel: '2' },
+          { menuCode: 'm28000', menuName: '危险废物管理计划', menuLevel: '2' },
+          { menuCode: 'm30000', menuName: '系统设置', menuLevel: '1' },
+          { menuCode: 'm31000', menuName: '仓库设置', menuLevel: '2' },
+          { menuCode: 'm32000', menuName: '物流人员设置', menuLevel: '2' },
+          { menuCode: 'm36000', menuName: '联单提报', menuLevel: '2' },
+          { menuCode: 'm33000', menuName: '系统账号设置', menuLevel: '2' },
+          { menuCode: 'm35000', menuName: '企业年审提报', menuLevel: '2' },
+          { menuCode: 'm34000', menuName: '企业资料修改', menuLevel: '2' },
+        ],
+      }
+    },
   },
 
   // user logout
   {
-    url: '/vue-admin-template/user/logout',
-    type: 'post',
-    response: _ => {
-      return {
-        code: 20000,
-        data: 'success'
-      }
-    }
-  }
+    url: '/corp/user/logout',
+    type: 'get',
+    response: (_) => {
+      // return {
+      //   code: 20000,
+      //   data: 'success',
+      // }
+      return true
+    },
+  },
+
+  {
+    url: '/anno/getRegCaptcha.*',
+    type: 'get',
+    response: () => {
+      // eslint-disable-next-line quotes
+      return `data:text/xml;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAAAyCAIAAACWMwO2AAACc0lEQVR42u3bP0sDMRjH8W4Khc7q4CAodOroIvgOHOwgSgcnO4kIOoiK6GvQzUEE6eLi4jsQF0EHX4IuDuJSiuDgycF53F2T9HpJ7pLvj2cxBv/1w5PnQq19E6IhNf4EBFgEWARYxLF0B/10OQXraa0Xlo4vvn80lS5Iicu1jhUJK4RaJil4qcAyZsvaUZgbmVQVsKTgmLHksDI3MGZZt1WrrqpwZXFrNSr0qNsSbDufWQhrLFiP891Kw4onjgxnuWGlheWg9texKmRrpEEKZGPCElPzF1YmMmAZnbGqYotHv0JgmXsqLC0slcsF7hrK1q7+YfUv7qOKPpf4ML7NW1gvSw9RjbTfq3YlgiVeMWYLWFVsV0NhJQwJFj2csdRteatKBEuFF7DKD8vWGxxqgmNOuggsARqfVQFLY9OyDsuiKqWnQpVFYJW5XVn57sDSchqWp13Z+gGApaVp2YVlXRWwtMBCFbC02LIIy+7ADiyNsGhXlYe1fXwbVFVgbXy+R+V8u3KhY4W8bCFL2FJsV5qQqfyLjun3Y7lxFJpHlg+WgY4FLKU0V2ZHnbHMCEtcaMVhXZ3WCylgabcVVIHD+8nhsrhy2/ommbAm1pthef5UKJUX4gNWIq12KyjVjiXW5ud1g/RCi45VmeE9fTgCq9SwBOl1Lkv+OzTmJoPitQQWARZxIoPns6iARbQjkzqbbu/lK2AROtaQXHcOghJs+GnshMXrDaziecWFQQ1YeXhJhQ2jBggHYX3UNy0Ko6W53LGKtaV+RDKfuX8UBrZKyMuB3O1+jVkuzFg6bBFpXm/eomJ4J9qR5QAHLKIFHLCIlmP0F2iDtsQq5eKHAAAAAElFTkSuQmCC`
+    },
+  },
 ]
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,147 +1,211 @@
 <template>
   <div class="login-container">
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
-    >
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
+    <div class="img-box">
+      <div class="left-img"></div>
+      <div class="right-form">
+        <el-form
+          ref="loginForm"
+          :model="loginForm"
+          :rules="loginRules"
+          class="login-form"
+          auto-complete="off"
+          label-position="left"
+        >
+          <div class="logo-box">
+            <img src="~@/assets/logo.png" alt="" />
+            电池回收平台
+          </div>
+          <div class="title-container">
+            <h3 class="title">系 统 登 录</h3>
+          </div>
+
+          <el-form-item prop="principal">
+            <span class="svg-container">
+              <svg-icon icon-class="user" />
+            </span>
+            <el-input v-model="loginForm.principal" placeholder="请输入用户名" type="text" auto-complete="off" />
+          </el-form-item>
+
+          <el-form-item prop="credentials">
+            <span class="svg-container">
+              <svg-icon icon-class="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              v-model="loginForm.credentials"
+              :type="passwordType"
+              placeholder="请输入密码"
+              auto-complete="off"
+              @keyup.enter.native="handleLogin"
+            />
+            <span class="show-pwd" @click="showPwd">
+              <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            </span>
+          </el-form-item>
+
+          <el-form-item prop="validCode">
+            <span class="svg-container">
+              <!-- <svg-icon icon-class="password" /> -->
+              <i class="el-icon-key"></i>
+            </span>
+            <el-input
+              v-model="loginForm.validCode"
+              placeholder="请输入验证码"
+              auto-complete="off"
+              @keyup.enter.native="handleLogin"
+              maxLength="4"
+            />
+            <span class="show-pwd" style="top: 0; right: 0; height: 40px">
+              <img class="security-img" style="height: 40px; width: 100px" @click="reloadSecu" :src="securitySrc" />
+            </span>
+          </el-form-item>
+
+          <div class="mb-15 fs-14 flex justify-between">
+            <el-checkbox v-model="isRemember">记住密码</el-checkbox>
+            <el-button type="text" @click="toForget" class="fs-14 pt-1">忘记密码</el-button>
+          </div>
+
+          <el-button
+            type="primary"
+            size="default"
+            style="width: 100%; margin-bottom: 16px"
+            :loading="loading"
+            @click.native.prevent="handleLogin"
+            >登录</el-button
+          >
+          <div class="text-right">
+            <el-button type="text" @click="toRegist" class="fs-14">公司注册</el-button>
+          </div>
+          <!-- <div class="tips">
+            <span style="margin-right: 20px">principal: admin</span>
+            <span> credentials: any</span>
+          </div> -->
+        </el-form>
       </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon
-            :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"
-          />
-        </span>
-      </el-form-item>
-
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width: 100%; margin-bottom: 30px"
-        @click.native.prevent="handleLogin"
-        >Login</el-button
-      >
-
-      <div class="tips">
-        <span style="margin-right: 20px">username: admin</span>
-        <span> password: any</span>
-      </div>
-    </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
+import { httpgetRegCaptcha } from '@/api/user'
+
+// import { validUsername } from '@/utils/validate'
+
+import { guid, blobToBase64 } from '@/utils/tools'
+import { title as project_title } from '@/settings'
 
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
+    // const validatePassword = (rule, value, callback) => {
+    //   if (value.length < 6) {
+    //     callback(new Error('The password can not be less than 6 digits'))
+    //   } else {
+    //     callback()
+    //   }
+    // }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        // principal: 'hzy',
+        // credentials: '123456',
+        principal: 'admin',
+        credentials: 'admin',
+        grantType: '0', // 密码登录
+        sessionUUID: '',
+        validCode: '1111', // 验证码
       },
       loginRules: {
-        username: [
-          { required: true, trigger: 'blur', validator: validateUsername }
-        ],
-        password: [
-          { required: true, trigger: 'blur', validator: validatePassword }
-        ]
+        principal: [{ required: true, trigger: 'blur', message: '请输入用户名' }],
+        credentials: [{ required: true, trigger: 'blur', message: '请输入密码' }],
+        validCode: [{ required: true, trigger: 'blur', message: '请输入验证码' }],
       },
       loading: false,
       passwordType: 'password',
-      redirect: undefined
+      redirect: undefined,
+
+      securitySrc: '',
+      isRemember: false, // 是否记住密码
     }
+  },
+  created() {
+    this.getUuid()
+
+    const objStr = window.localStorage.getItem(project_title)
+    if (!objStr) return
+    this.isRemember = true
+    const pwdObj = JSON.parse(window.atob(objStr))
+    this.loginForm.principal = pwdObj.principal
+    this.loginForm.credentials = pwdObj.credentials
   },
   watch: {
     $route: {
       handler: function (route) {
         this.redirect = route.query && route.query.redirect
       },
-      immediate: true
-    }
+      immediate: true,
+    },
+    isRemember(newV) {
+      if (!newV) window.localStorage.removeItem(project_title)
+    },
   },
   methods: {
+    // principal, credentials
     showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
-      } else {
-        this.passwordType = 'password'
-      }
-      this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
+      this.passwordType = this.passwordType ? '' : 'password'
     },
     handleLogin() {
       this.$refs.loginForm.validate((valid) => {
-        if (valid) {
-          this.loading = true
-          this.$store
-            .dispatch('user/login', this.loginForm)
-            .then(() => {
+        if (!valid) return
+        this.loading = true
+        this.$store
+          .dispatch('user/login', this.loginForm)
+          .then(() => {
+            // this.$router.push({ path: this.redirect || '/' })
+            this.reloadSecu()
+            const userStatus = this.$store.state.user.userStatus
+            this.loading = false
+            // 根据登录接口的返回 userStatus 判断
+            // 0:正常 1 未提交认证 2 未认证
+            // 0：正常进系统
+            // 1：跳转到认证页面
+            // 2：跳转到未认证的插图页面
+            if (userStatus === '0') {
+              this.isRemember && localStorage.setItem(project_title, window.btoa(JSON.stringify(this.loginForm)))
               this.$router.push({ path: this.redirect || '/' })
-              this.loading = false
-            })
-            .catch(() => {
-              this.loading = false
-            })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
+            } else if (userStatus === '1') {
+              this.$router.push('/authority')
+            } else if (userStatus === '2') {
+              this.$router.push('/waiting')
+            }
+          })
+          .catch(() => {
+            this.reloadSecu()
+            this.loginForm.validCode = ''
+            this.loading = false
+          })
       })
-    }
-  }
+    },
+    getUuid() {
+      this.loginForm.sessionUUID = guid()
+      // 获取二维码
+      httpgetRegCaptcha({ uuid: this.loginForm.sessionUUID }).then((res) => {
+        blobToBase64(res).then((msg) => {
+          // this.securitySrc = msg
+          this.securitySrc =
+            'data:text/xml;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAAAyCAIAAACWMwO2AAACnUlEQVR42u3cu0oDQRQG4PUBLCwtBLGxES0ECQRsFGzEQkFS2lpYChYWNkpeQSsbEUQDIjaW1nYi2qWwtxdBXBkZJpmLu7NzOTP5f06TLBuSzceZMwtJ8YkgHlLgEiCAhQAWAlgIAlgIYCHecnm9wkp5tDX/V4AVJwv9Vll5CFM648LCO0PHyoFX3U4WgBpgZcirevw5Ayzw8jKcFafbG7yqn2ZxivPsLq/xAi9yu0LAcs6rPdbjBVjDSsTnDUczhmUXURVgJQ/rovfMi44qwEoSli4issDaAGtgVyjrER+aj6aSAMhkVYDlHtb69MdIXUdREmD9wup29gGLIKzbyXFWFD7d0sG9XLU7lvlheFjikK4rOqqcdywujAgyszYOzgRLOcg3h3V4884qD1iyISyFcWBl1rEIwjp+2eSle14+6h6WaEUcucywzK8bYMaiAEsJyCGsq5OjsprDkklVh/XdOiurKSy721pREh2WTo/zjsV4VUc2REenqlbHYryqIysqugGsiLDqIjMbar4UVhEGWI5VzXUmgs1YOmHmzuRwxjIIAyyXquSOVTorK+LwLuvxMbzLC6UalvJkwLKAxdsYr4i7wjBhvArd1s8Mi8Kdkliw/l3mzEuhiMyTs7iwBjoWYFnAqlvKV/OBDLAAS40sK1jJJT9YLKt3W4A10vex7GasALYAC7C82AIswPJiC7AAy4stwAIsLwEswAIs5RWceS0LsJzD2uvOWlRuHYvxCi+MLKzoyW0pDCwMsBzC6j7usCI9YwUQRlbVeXsqaZRcWENnfof35ryUdEj99is/W06cUd8V0v9RYfa27FbM5GGRfeelrVx55dCxDLySeOcjawv/QQpb7vP0tghYCDqWizx89VnhuwcsJL38AJDT0UuCRPOaAAAAAElFTkSuQmCC'
+        })
+      })
+    },
+    reloadSecu() {
+      this.getUuid()
+    },
+    toRegist() {
+      this.$router.push('/regist')
+    },
+    toForget() {
+      this.$router.push('/forget')
+    },
+  },
 }
 </script>
 
@@ -151,7 +215,7 @@ export default {
 
 $bg: #283443;
 $light_gray: #fff;
-$cursor: #fff;
+$cursor: #283443;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
@@ -159,21 +223,19 @@ $cursor: #fff;
   }
 }
 
-/* reset element-ui css */
 .login-container {
   .el-input {
     display: inline-block;
-    height: 47px;
+    height: 35px;
     width: 85%;
 
     input {
       background: transparent;
       border: 0px;
-      -webkit-appearance: none;
       border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
+      padding: 6px 5px 6px 15px;
+      // color: $light_gray;
+      // height: 47px;
       caret-color: $cursor;
 
       &:-webkit-autofill {
@@ -198,23 +260,55 @@ $dark_gray: #889aa4;
 $light_gray: #eee;
 
 .login-container {
-  min-height: 100%;
+  position: relative;
+  height: 100vh;
+  min-width: 800px;
+  min-height: 600px;
   width: 100%;
-  background-color: $bg;
-  overflow: hidden;
+  background: $bg url(~@/assets/login-bg1.png) no-repeat;
+  background-size: 100% 100%;
+  // overflow: hidden;
+  .logo-box {
+    font-size: 18px;
+    padding-bottom: 5vh;
+    img {
+      vertical-align: middle;
+      width: 60px;
+    }
+  }
+  .img-box {
+    position: absolute;
+    display: flex;
+    width: 80%;
+    height: 80%;
+    min-width: 700px;
+    min-height: 500px;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background: #fff;
+    .left-img {
+      flex: 4;
+      background: $bg url(~@/assets/car-bg.png) no-repeat;
+      background-size: 100% 100%;
+    }
+    .right-form {
+      flex: 3;
+    }
+  }
 
   .login-form {
     position: relative;
-    width: 520px;
+    // width: 520px;
     max-width: 100%;
-    padding: 160px 35px 0;
+    padding: 20px 35px 0;
     margin: 0 auto;
     overflow: hidden;
   }
 
   .tips {
     font-size: 14px;
-    color: #fff;
+    // color: #fff;
     margin-bottom: 10px;
 
     span {
@@ -236,8 +330,8 @@ $light_gray: #eee;
     position: relative;
 
     .title {
-      font-size: 26px;
-      color: $light_gray;
+      font-size: 24px;
+      color: #092b43;
       margin: 0px auto 40px auto;
       text-align: center;
       font-weight: bold;
